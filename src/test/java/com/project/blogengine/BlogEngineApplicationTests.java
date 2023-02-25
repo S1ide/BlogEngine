9016338f9f9bc481aadef448cb241ac57dbbc022
Kopotev Nikita <122271491+viaarm@users.noreply.github.com>
2023-02-25 17:11:09 +0500

    Initial commit

---
+++ src/test/java/com/project/blogengine/BlogEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.blogengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
